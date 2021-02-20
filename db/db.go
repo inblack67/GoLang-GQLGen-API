04b0e7fc343eff2cda4e2ad422289e1cf79641d0
Aman Bhardwaj <inblack>
2021-02-20 21:36:39 +0530

    log

--- db/db.go
+++ db/db.go
@@ -1,10 +1,15 @@
 package db
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"github.com/fatih/color"
 	"github.com/inblack67/GQLGenAPI/mymodels"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -14,12 +19,28 @@ var (
 
 // ConnectDB ...
 func ConnectDB() (*gorm.DB){
+
 	dsn := "host=localhost user=postgres password=postgres dbname=gographql port=5432"
+
 	var err error
-	PgConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), 
+		logger.Config{
+			SlowThreshold: time.Microsecond, 	// to make it all queries log  
+			LogLevel:      logger.Info, 
+			Colorful:      true,         
+		},
+	)
+
+	PgConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
 	if err != nil{
 		panic(err)
 	}
+
 	PgConn.AutoMigrate(&mymodels.User{}, &mymodels.Story{})
 	color.Green("Postgres is here")
 	return PgConn
